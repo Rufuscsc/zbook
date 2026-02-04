3cfca323633224de6eeb0c403b0ae3f45534d066
rufuscsc <rufusmfmwellens>
2026-02-04 10:37:37 +0100

    update book page code

--- app/add-book/page.tsx
+++ app/add-book/page.tsx
@@ -2,7 +2,7 @@ import AddBookform from "@/components/add-book/AddBookForm";
 
 const AddBook = ()=>{
     return(
-        <div className="py-5 min-h-screen px-4 bg-white md:bg-amber-50">
+        <div className="py-5 min-h-screen px-4 bg-background">
             <h1 className="text-5xl! md:text-3xl font-bold text-center mx-auto text-foreground">Add a New Books</h1>
             <p className="text-lg text-[#847062] text-center leading-relaxed" >Share a literary treasure with our community</p>
 
--- app/book/[bookId]/edit/page.tsx
+++ app/book/[bookId]/edit/page.tsx
@@ -1,8 +1,21 @@
+import EditBookForm from '@/components/EditBookForm'
 import React from 'react'
 
-function EditBook() {
+const EditBook = async ({ params }: { params: Promise<{ bookId: string }> }) => {
+  const { bookId } = await params;
   return (
-    <div>EditBook</div>
+    <div className="py-5 bg-background min-h-screen">
+      <h1 className="text-5xl! md:text-3xl font-bold text-foreground text-center">
+        Edit Book
+      </h1>
+
+      <p className="text-base text-[#847062] leading-relaxed text-center">
+        Update the details of your literary treasure
+      </p>
+
+      {/* Form */}
+      <EditBookForm bookId={bookId} />
+    </div>
   )
 }
 
